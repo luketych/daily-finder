1a569385b9609bc14d1203de7fcb746f848745e0
luketych <luke.tych@gmail.com>
2023-04-23 15:27:07 -0700

    multi create
    Former-commit-id: 2f8d41f9a60fb5321600fef1ca1eb3c19b0ddb28 [formerly 220d7887afd081bb9463603eb9baa47a913ff1b6 [formerly 0006656fa2dbfd334ba272b1985587157ab87291]]
    Former-commit-id: 220d7887afd081bb9463603eb9baa47a913ff1b6
    Former-commit-id: 0e56f530dc2b7514b06c84c211e7360ae01003e8

--- src/services/articles/articles.class.js
+++ src/services/articles/articles.class.js
@@ -6,6 +6,7 @@ export class ArticlesService extends MongoDBService {}
 export const getOptions = (app) => {
   return {
     paginate: app.get('paginate'),
+    multi: [ 'create' ],
     Model: app.get('mongodbClient').then((db) => db.collection('articles'))
   }
 }
