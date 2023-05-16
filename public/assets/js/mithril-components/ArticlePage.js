d755c272e71e371bf5c055dc0d2c7d78e24f1cfa
luketych <luke.tych@gmail.com>
2023-05-15 20:32:37 -0700

    Trying to get adtracking stuff to start loading once everything on page has finished loading.

--- public/assets/js/mithril-components/ArticlePage.js
+++ public/assets/js/mithril-components/ArticlePage.js
@@ -75,28 +75,46 @@ export default {
     },
 
     oncreate: function(vnode) {
-      window.onload = function() {
-          console.log("window.onload");
-          var script = document.createElement('script');
-          script.src = 'https://trk.dailyfinder.org/unilpclick.js?attribution=lastpaid&cookiedomain=dailyfinder.org&cookieduration=90&defaultcampaignid=6456c81c70175b0001e0f1d1&regviewonce=false';
+      // Wrap the oncreate function in a promise
+      var oncreatePromise = new Promise(function(resolve, reject) {
+        console.log("oncreate");
+        var script = document.createElement('script');
+        script.src = 'https://trk.dailyfinder.org/unilpclick.js?attribution=lastpaid&cookiedomain=dailyfinder.org&cookieduration=90&defaultcampaignid=6456c81c70175b0001e0f1d1&regviewonce=false';
 
-          // Listen for the 'load' event on the script element
-          script.addEventListener('load', function() {
-              console.log('Script has finished loading!');
-              console.log(getCookie('rtkclickid-store'))
+        // Listen for the 'load' event on the script element
+        script.addEventListener('load', function() {
+          console.log('Script has finished loading!');
+          console.log(getCookie('rtkclickid-store'))
 
-              const relatedTopicsAdsEl = document.querySelector('.related-topics-ads a')
+          const relatedTopicsAdsEl = document.querySelector('.related-topics-ads a')
 
-              const href = relatedTopicsAdsEl.getAttribute('href')
-              if (!href.includes('clickid') && !href.includes('refferer')) {
-                relatedTopicsAdsEl.setAttribute('href', href + `?clickid=${getCookie('rtkclickid-store')}&refferer=${window.location.href}`)
-              }
-
-          });
+          const href = relatedTopicsAdsEl.getAttribute('href')
+          if (!href.includes('clickid') && !href.includes('refferer')) {
+            relatedTopicsAdsEl.setAttribute('href', href + `?clickid=${getCookie('rtkclickid-store')}&refferer=${window.location.href}`)
+          }
           
-          document.body.appendChild(script);
-      };
+          // Resolve the promise when the script has loaded
+          resolve();
+        });
+        
+        document.body.appendChild(script);
+      });
+
+      // Wrap the DOMContentLoaded event in a promise
+      var domContentLoadedPromise = new Promise(function(resolve, reject) {
+        document.addEventListener('DOMContentLoaded', function() {
+          console.log('DOM is fully loaded');
+          resolve();
+        });
+      });
+
+      // Use Promise.all to run code once both promises have resolved
+      Promise.all([oncreatePromise, domContentLoadedPromise]).then(function() {
+        console.log('Both oncreate and DOMContentLoaded have finished');
+        // You can put additional code here that you want to run after both events have finished
+      });
     }
+
 }
 
 
